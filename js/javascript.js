b480c80886e4d082d3931a2d4c9fb8d0b27e58a1
EdelDenis <edeldenis12@gmail.com>
2022-04-21 09:31:42 +0000

    j

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+"use strict";
+
+console.log("arr" + " - object");
+console.log(4 + "5");
+
+let incr = 10;
+let decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(2+2*2 !== "6");
+
+

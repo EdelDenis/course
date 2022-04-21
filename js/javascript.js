2e23e3f42a00a4713184a03fc51f1e3d7fc180bc
EdelDenis <edeldenis12@gmail.com>
2022-04-21 10:41:06 +0000

    h

--- js/javascript.js
+++ js/javascript.js
@@ -4,19 +4,25 @@
 
 
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {
-      lastSeenMovie: movieAward
+      
     },
-    actors: {
-        
-    },
+    actors: {},
     genres: [],
     privat: false
 };
+let lastSeenMovie1 = prompt("Один из последних просмотреных фильмов?", "");
+let movieAward1 = prompt("На сколько оцените его?", "");
+let lastSeenMovie2 = prompt("Один из последних просмотреных фильмов?", "");
+let movieAward2= prompt("На сколько оцените его?", "");
 
-let lastSeenMovie = prompt("Один из последних просмотреных фильмов?");
-let movieAward = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[lastSeenMovie1] = movieAward1;
+personalMovieDB.movies[lastSeenMovie2] = movieAward2;
+
+console.log(personalMovieDB);
 

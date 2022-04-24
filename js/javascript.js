0905becd8ebbe42499b8a650c64a1c23eecf8e7b
EdelDenis <edeldenis12@gmail.com>
2022-04-24 10:42:42 +0000

    g

--- js/javascript.js
+++ js/javascript.js
@@ -231,3 +231,67 @@ for (let i = 2; i <= 16; i++) {
 
 /* ------------ФУНКЦИИ------------*/
 
+
+/*let num = 20;
+function showFirstMessage (text) {
+    console.log(text);
+    console.log(num);
+}
+
+showFirstMessage("Hello world");
+console.log(num);
+
+function calc (a,b) {
+    return (a + b);
+}
+
+console.log(calc(7,5));
+
+function ret () {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("Hello");
+};
+
+logger();*/
+
+const calc = (a,b) => {
+    console.log("1");
+    return a + b;
+};
+
+const usdCurr = 28;
+
+const euroCurr = 32;
+const discount = 0.9;
+
+function convert (amount, curr) {
+    return curr * amount;
+}
+
+function promotion (result) {
+    console.log(result * discount);
+}
+const res = convert(500,usdCurr);
+promotion (res);
+
+function test() {
+    for(let i =0;i<5;i++) {
+        console.log(i);
+        if (i === 3) {
+            return
+        }
+        console.log("Done");
+    }
+}
+
+test ();
+
+function doNothing () {};
+console.log(doNothing() === undefined);

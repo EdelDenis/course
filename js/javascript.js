6cfe7bc359500811cc84f9cde635e68f19a5f5b9
EdelDenis <edeldenis12@gmail.com>
2022-04-24 11:11:00 +0000

    j

--- js/javascript.js
+++ js/javascript.js
@@ -295,3 +295,32 @@ test ();
 
 function doNothing () {};
 console.log(doNothing() === undefined);
+
+/*-----------МЕТОДЫ СТРОК И ЧИСЛЕ------------*/
+
+const str = "test";
+const arr = [1,2,3];
+
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+//console.log(logg.slice(6));
+
+//console.log(logg.substring(6,11));
+
+console.log(logg.substr(6,5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+

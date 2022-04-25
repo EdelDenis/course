26bc9e3f7d78e915c37cb84ab1a539e6b7d9203d
EdelDenis <edeldenis12@gmail.com>
2022-04-25 11:19:02 +0000

    g

--- js/javascript.js
+++ js/javascript.js
@@ -266,33 +266,38 @@ const calc = (a,b) => {
     return a + b;
 };
 
+let usdCurr = 28;
+const discount = 0.9;
+
+function convert (amount,curr) {
+    return amount * curr;
+}
+
+function promotion(result){
+    console.log(result * discount);
+}
+
+const res = convert(500,usdCurr)
+promotion(res);
+
+function test() {
+    for(let i = 0; i<5; i++){
+        console.log(i);
+        if(i === 3) return
+    }
+    console.log("Done");
+}
+
+test();
+
+function doNothing () {};
+    console.log(doNothing() === undefined);
+
 
 
 /*-----------МЕТОДЫ СТРОК И ЧИСЛЕ------------*/
 
-const str = "test";
-const arr = [1,2,3];
 
-console.log(str.toLowerCase());
-console.log(str);
-
-const fruit = "Some fruit";
-
-console.log(fruit.indexOf("fruit"));
-
-const logg = "Hello world";
-//console.log(logg.slice(6));
-
-//console.log(logg.substring(6,11));
-
-console.log(logg.substr(6,5));
-
-const num = 12.2;
-console.log(Math.round(num));
-
-const test = "12.2px";
-console.log(parseInt(test));
-console.log(parseFloat(test));
 
 
 

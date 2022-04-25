c86146e8d4cef9ad922387789aebeec29d7df37a
EdelDenis <edeldenis12@gmail.com>
2022-04-25 14:59:07 +0000

    j

--- js/javascript.js
+++ js/javascript.js
@@ -326,7 +326,32 @@ console.log(parseFloat(test));
 
 /* ----------------CALL BACK ФУНКЦИИ----------------*/
 
+function first () {
+    // Do smthg
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+    }
 
+    function second () {
+        console.log(2);
+    }
+
+    first();
+    second();
+    
+    function learnJS(lang, callback){
+         console.log("Я учу: " + lang)
+         callback();
+    }
+
+    function done() {
+        console.log("Я прошел этот урок!");
+    }
+
+    learnJS("JavaScript", done);
+
+/* -----------Объекты, деструктуризация объектов (ES6)------------*/
 
 
 

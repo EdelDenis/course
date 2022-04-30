3f40763e41f50fcc5f561bc514916ea93e4741f2
EdelDenis <edeldenis12@gmail.com>
2022-04-30 08:11:33 +0000

    j

--- js/javascript.js
+++ js/javascript.js
@@ -498,3 +498,36 @@ const add = {
 };
 
 console.log(Object.assign(numbers, add));
+
+//-------------------- ОСНОВЫ ООП --------------------
+
+let stre = "some";
+let strObj = new String(stre);
+
+console.log(typeof(stre));
+console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+const soldier = {
+    health: 400,
+    armor:100,
+    sayHello: function() {
+        console.log("Hello");
+    }
+};
+
+const john = Object.create(soldier);
+
+/*const john = {
+    health: 100
+};*/
+
+// john.__proto__ = soldier; - старая версия
+
+// Object.setPrototypeOf(john, soldier); - джону присваивается прототип солдат
+
+john.sayHello();
+
+console.log(john.armor);
+
